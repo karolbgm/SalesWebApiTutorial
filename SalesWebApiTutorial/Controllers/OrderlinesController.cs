7d593135110c7b824e2462adc64da21f62e225f6
Karol Morgan <karolyne.morgan@outlook.com>
2024-06-28 15:54:21 -0400

    final commit

--- SalesWebApiTutorial/Controllers/OrderlinesController.cs
+++ SalesWebApiTutorial/Controllers/OrderlinesController.cs
@@ -23,30 +23,83 @@ namespace SalesWebApiTutorial.Controllers
         }
 
         //GET api/Orderlines/Total/{id}
-        [HttpGet("Total/{orderId}")]
-        public async Task<ActionResult<Order>> GetOrderTotal(int orderId)
-        {
-            var myOrder = await _context.Orders.FindAsync(orderId);
+        //[HttpGet("Total/{orderId}")]
+        //public async Task<ActionResult<Order>> GetOrderTotal(int orderId)
+        //{
+        //    var myOrder = await _context.Orders.FindAsync(orderId); //we read it, so it's in the cache
 
-            if (myOrder == null)
+        //    if (myOrder == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var total = await (from o in _context.Orders
+        //                       join ol in _context.Orderlines on o.Id equals ol.OrderId
+        //                       join i in _context.Items on ol.ItemId equals i.Id
+        //                       where o.Id == orderId //INSTEAD OF DOING A LOOP, USE WHERE CLAUSE AND SELECT!!
+        //                       select ol.Quantity * i.Price).SumAsync();
+
+        //    myOrder.Total = total; //and the cache understands this was changed
+
+        //    /*_context.Entry(myOrder).State = EntityState.Modified;*/ //we don't need to do this because we read order and it's in the cache
+        //    await _context.SaveChangesAsync();
+
+        //    return myOrder;
+        //}
+
+        //GREG'S METHOD
+        //No Http Method - user don't call it 
+
+        //this method will be inside ADD, UPDATE AND DELETE
+        private async Task<IActionResult> RecalculateOrderTotal(int orderId) //it doesn't return data
+            //use IACTIONRESULT when you don't want to return data
+            //IActionResult is an interface, doesn't need a type and that's what we want because we don't want to return data!
+            //ActionResult is a class, it needs a Type
+            //method was private so the user can't access it directly
+        {
+
+
+            // read the order to be recalculated
+
+            var order = await _context.Orders.FindAsync(orderId); //we have to read it so we can update it
+
+
+            // if the order is not found, return NOT FOUND
+
+            if (order is null)
             {
+
                 return NotFound();
+
             }
 
-            var total = await (from o in _context.Orders
-                               join ol in _context.Orderlines on o.Id equals ol.OrderId
-                               join i in _context.Items on ol.ItemId equals i.Id
-                               where o.Id == orderId //INSTEAD OF DOING A LOOP, USE WHERE CLAUSE AND SELECT!!
-                               select ol.Quantity * i.Price).SumAsync();
+            // if we get here, we did find the order
 
-            myOrder.Total = total;
+            // now calculate the total and store it in order.Total
 
-            _context.Entry(myOrder).State = EntityState.Modified;
+            order.Total = (from ol in _context.Orderlines
+
+                           join i in _context.Items
+
+                             on ol.ItemId equals i.Id
+
+                           where ol.OrderId == orderId
+
+                           select new //the select helps us to select what piece of data we want, hence the TYPE we want
+                           {
+
+                               LineTotal = ol.Quantity * i.Price //it will store in this column called LineTotal -- we'll have a collection of LineTotal
+
+                           }).Sum(x => x.LineTotal); //sum will return the line total of all of them together
+
+
+            // now update the order with the new Total
+
             await _context.SaveChangesAsync();
 
-            return myOrder;
-        }
+            return Ok();
 
+        }
         // GET: api/Orderlines
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Orderline>>> GetOrderlines()
@@ -80,9 +133,10 @@ namespace SalesWebApiTutorial.Controllers
 
             _context.Entry(orderline).State = EntityState.Modified;
 
-            try
+            try //try catch is used here to make sure two people don't try to update it at the same time
             {
                 await _context.SaveChangesAsync();
+                await RecalculateOrderTotal(orderline.OrderId); //we're gonna recalculate the order after the user makes changes
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -106,6 +160,7 @@ namespace SalesWebApiTutorial.Controllers
         {
             _context.Orderlines.Add(orderline);
             await _context.SaveChangesAsync();
+            await RecalculateOrderTotal(orderline.OrderId);
 
             return CreatedAtAction("GetOrderline", new { id = orderline.Id }, orderline);
         }
@@ -122,6 +177,7 @@ namespace SalesWebApiTutorial.Controllers
 
             _context.Orderlines.Remove(orderline);
             await _context.SaveChangesAsync();
+            await RecalculateOrderTotal(orderline.OrderId);
 
             return NoContent();
         }
